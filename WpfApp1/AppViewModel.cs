2025cc6f98c539e4b1b5d9b974340c6f23f844a2
Ron-joan <56474239+Ron-joan@users.noreply.github.com>
2021-11-02 15:11:38 +0800

    调整项目设置

---
+++ WpfApp1/AppViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class AppViewModel : ReactiveObject
+    {
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get => _searchTerm;
+            set => this.RaiseAndSetIfChanged(ref _searchTerm, value);
+        }
+    }
+}
